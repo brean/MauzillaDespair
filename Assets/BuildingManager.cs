b9bd4a50091c7ebff6a96f422fd98b3ca3443569
Andreas Bresser <self@andreasbresser.de>
2020-02-01 17:31:15 +0100

    Damaged building for tile testing

---
+++ Assets/BuildingManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BuildingManager : MonoBehaviour
+{
+    public TileBase damageTile;
+    public TileBase origTile;
+    public Tilemap groundLevelMap;
+    public Vector3Int tilePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // origTile = groundLevelMap.GetTile(tilePosition);
+        //groundLevelMap.SetTile(tilePosition, damageTile);
+        // groundLevelMap.getTile()
+        // groundLevelMap.setTile()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
